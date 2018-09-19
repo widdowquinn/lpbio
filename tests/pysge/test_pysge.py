29a1e49e0eb5f3214827181a4c15f7e7503594d2
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2018-09-19 14:07:37 +0100

    add dependencies in SGE tests with job/jobgroup methods

--- tests/pysge/test_pysge.py
+++ tests/pysge/test_pysge.py
@@ -10,36 +10,43 @@ import pysge
 
 def test_create_job():
     """Create Job for SGE-like scheduler"""
-    job = pysge.Job(name="test_job", command="echo {}".format(time.asctime()))
+    pysge.Job(name="test_job", command="echo {}".format(time.asctime()))
 
 
 def test_create_job_dependencies():
     """Create Job with dependencies for SGE-like scheduler"""
-    job = pysge.Job(name="test_job_dependencies", command="echo {}".format(time.asctime()))
-    job.dependencies = [
+    job = pysge.Job(
+        name="test_job_dependencies", command="echo {}".format(time.asctime())
+    )
+    depjobs = [
         pysge.Job(
             name="dependency {}".format(i), command="echo {}".format(time.asctime())
         )
         for i in range(3)
     ]
+    [job.add_dependency(depjob) for depjob in depjobs]
 
 
 def test_create_jobgroup():
     """Create parameter-sweep JobGroup for SGE-like scheduler"""
     args = {"arg1": ["a", "b", "c"]}
-    jobgroup = pysge.JobGroup(name="test_jobgroup", command="echo", arguments=args)
+    pysge.JobGroup(name="test_jobgroup", command="echo", arguments=args)
 
 
 def test_create_jobgroup_dependencies():
     """Create parameter-sweep JobGroup with dependencies for SGE-like scheduler"""
     args = {"arg1": ["a", "b", "c"]}
-    jobgroup = pysge.JobGroup(name="test_jobgroup_dependencies", command="echo", arguments=args)
-    jobgroup.dependencies = [
+    jobgroup = pysge.JobGroup(
+        name="test_jobgroup_dependencies", command="echo", arguments=args
+    )
+    depjobs = [
         pysge.Job(
             name="dependency {}".format(i), command="echo {}".format(time.asctime())
         )
         for i in range(3)
     ]
+    for depjob in depjobs:
+        jobgroup.add_dependency(depjob)
 
 
 @pytest.mark.skipif(
@@ -59,7 +66,9 @@ def test_create_run_job():
 def test_create_run_jobgroup():
     """Create and run JobGroup with SGE-like scheduler"""
     args = {"arg1": ["a", "b", "c"]}
-    jobgroup = pysge.JobGroup(name="test_run_jobgroup", command="echo", arguments=args)
+    jobgroup = pysge.JobGroup(
+        name="test_run_jobgroup", command="echo $arg1", arguments=args
+    )
     pysge.build_and_submit_jobs(jobgroup)
 
 
@@ -70,13 +79,17 @@ def test_create_run_jobgroup():
 )
 def test_create_run_job_dependencies():
     """Create and run Job with dependencies for SGE-like scheduler"""
-    job = pysge.Job(name="test_run_job_dependencies", command="echo {}".format(time.asctime()))
-    job.dependencies = [
+    job = pysge.Job(
+        name="test_run_job_dependencies", command="echo {}".format(time.asctime())
+    )
+    depjobs = [
         pysge.Job(
             name="dependency {}".format(i), command="echo {}".format(time.asctime())
         )
         for i in range(3)
     ]
+    for depjob in depjobs:
+        job.add_dependency(depjob)
     pysge.build_and_submit_jobs(job)
 
 
@@ -88,11 +101,15 @@ def test_create_run_job_dependencies():
 def test_create_run_jobgroup_dependencies():
     """Create parameter-sweep JobGroup with dependencies for SGE-like scheduler"""
     args = {"arg1": ["a", "b", "c"]}
-    jobgroup = pysge.JobGroup(name="test_run_jobgroup_dependencies", command="echo", arguments=args)
-    jobgroup.dependencies = [
+    jobgroup = pysge.JobGroup(
+        name="test_run_jobgroup_dependencies", command="echo $arg1", arguments=args
+    )
+    depjobs = [
         pysge.Job(
             name="dependency {}".format(i), command="echo {}".format(time.asctime())
         )
         for i in range(3)
     ]
+    for depjob in depjobs:
+        jobgroup.add_dependency(depjob)
     pysge.build_and_submit_jobs(jobgroup)
