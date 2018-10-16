4596ea7013b211c657cd97afac94cf9c74251392
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2018-10-16 10:26:06 +0100

    convert Swarm wrapper tests to unittest.TestCase

--- tests/swarm/test_swarm.py
+++ tests/swarm/test_swarm.py
@@ -4,6 +4,7 @@
 
 import os
 import shutil
+import unittest
 
 import pytest
 
@@ -22,63 +23,60 @@ TARGETDIR = os.path.join(TESTDIR, "targets")
 TARGETFILE = os.path.join(TARGETDIR, "swarm.out")
 
 
-def setup():
-    """Set up test fixtures"""
-    try:
-        shutil.rmtree(OUTDIR)
-    except FileNotFoundError:
-        pass
-    os.makedirs(OUTDIR, exist_ok=True)
+class TestSwarm(unittest.TestCase):
 
+    """Class collecting tests for Swarm wrapper."""
 
-def test_swarm_wrapper_creation():
-    """swarm executable is in $PATH"""
-    swarm.Swarm("swarm")
+    def setUp(self):
+        """Set up test fixtures"""
+        try:
+            shutil.rmtree(OUTDIR)
+        except FileNotFoundError:
+            pass
+        os.makedirs(OUTDIR, exist_ok=True)
 
+    def test_swarm_wrapper_creation(self):
+        """swarm executable is in $PATH"""
+        swarm.Swarm("swarm")
 
-def test_swarm_parameters():
-    parameters = swarm.SwarmParameters(t=1, d=1)
-    assert parameters.t == 1
-    assert parameters.d == 1
+    def test_swarm_parameters(self):
+        parameters = swarm.SwarmParameters(t=1, d=1)
+        self.assertEqual(parameters.t, 1)
+        self.assertEqual(parameters.d, 1)
 
+    def test_swarm_cmd(self):
+        """swarm module returns correct form of cmd-line"""
+        parameters = swarm.SwarmParameters(t=1, d=1)
+        cmd = swarm.build_cmd(INFILE, OUTFILE, parameters)
+        self.assertEqual(cmd, ["swarm", "-t 1", "-d 1", "-o", OUTFILE, INFILE])
 
-def test_swarm_cmd():
-    """swarm module returns correct form of cmd-line"""
-    parameters = swarm.SwarmParameters(t=1, d=1)
-    cmd = swarm.build_cmd(INFILE, OUTFILE, parameters)
-    assert cmd == ["swarm", "-t 1", "-d 1", "-o", OUTFILE, INFILE]
+    def test_swarm_wrapper_cmd(self):
+        """swarm wrapper returns correct form of cmd-line"""
+        cluster = swarm.Swarm("swarm")
+        target = ["swarm", "-t 1", "-d 1", "-o", OUTFILE, INFILE]
+        parameters = swarm.SwarmParameters(t=1, d=1)
+        self.assertEqual(cluster.run(INFILE, OUTDIR, parameters, dry_run=True), target)
 
+    def test_swarm_wrapper_exec_notexist(self):
+        """error thrown when swarm executable does not exist"""
+        with pytest.raises(LPBioNotExecutableError):
+            swarm.Swarm(os.path.join(".", "swarm"))
 
-def test_swarm_wrapper_cmd():
-    """swarm wrapper returns correct form of cmd-line"""
-    cluster = swarm.Swarm("swarm")
-    target = ["swarm", "-t 1", "-d 1", "-o", OUTFILE, INFILE]
-    parameters = swarm.SwarmParameters(t=1, d=1)
-    assert cluster.run(INFILE, OUTDIR, parameters, dry_run=True) == target
+    def test_swarm_wrapper_run(self):
+        """swarm clusters test data"""
+        cluster = swarm.Swarm("swarm")
 
+        parameters = swarm.SwarmParameters(t=1, d=1)
+        cluster.run(INFILE, OUTDIR, parameters)
 
-def test_swarm_wrapper_exec_notexist():
-    """error thrown when swarm executable does not exist"""
-    with pytest.raises(LPBioNotExecutableError) as e_info:
-        swarm.Swarm(os.path.join(".", "swarm"))
+    def test_swarm_output_parse(self):
+        """Swarm runs and output parses correctly"""
+        cluster = swarm.Swarm("swarm")
 
+        parameters = swarm.SwarmParameters(t=1, d=1)
+        result = cluster.run(INFILE, OUTDIR, parameters)
 
-def test_swarm_wrapper_run():
-    """swarm clusters test data"""
-    cluster = swarm.Swarm("swarm")
-
-    parameters = swarm.SwarmParameters(t=1, d=1)
-    cluster.run(INFILE, OUTDIR, parameters)
-
-
-def test_swarm_output_parse():
-    """Swarm runs and output parses correctly"""
-    cluster = swarm.Swarm("swarm")
-
-    parameters = swarm.SwarmParameters(t=1, d=1)
-    result = cluster.run(INFILE, OUTDIR, parameters)
-
-    parser = swarm.SwarmParser()
-    target = parser.read(TARGETFILE)
-    swarms = parser.read(result.outfilename)
-    assert target == swarms
+        parser = swarm.SwarmParser()
+        target = parser.read(TARGETFILE)
+        swarms = parser.read(result.outfilename)
+        self.assertEqual(target, swarms)
