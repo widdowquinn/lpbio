5f6c0d00920a7983b8c8ac286888ac736447f82e
Leighton Pritchard <leighton.pritchard@strath.ac.uk>
2019-11-07 12:54:54 +0000

    add gfa_to_fasta script
    Add script that converts assembly graph format (.gfa) to a FASTA
    file of edge sequences.

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setuptools.setup(
     url="http://widdowquinn.github.io/lpbio/",  # project home page
     download_url="https://github.com/widdowquinn/lpbio/releases",
     scripts=[os.path.join("bin", "bulk_prokka"),
-             os.path.join("bin", "climb_setup")],
+             os.path.join("bin", "climb_setup"),
+             os.path.join("bin", "gfa_to_fasta")],
     packages=setuptools.find_packages(),
     package_data={},
     include_package_date=True,
